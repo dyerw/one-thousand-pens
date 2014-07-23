4cd4959057eeb40a3b34d96a39ac776b6aaa3bdb
Liam Dyer <dyer.w@husky.neu.edu>
2014-07-22 19:07:41 -0500

    knocked up teh version a bit, we've come quite a ways

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='One Thousand Pens',
-      version='0.001',
+      version='0.01',
       description='communal storytelling site',
       author='Some dudes',
       package_dir={'':'src'},
