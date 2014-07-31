d84ba9dcdb171f48f841e653cb9501f79c6ae080
Liam Dyer <dyer.w@husky.neu.edu>
2014-07-30 21:56:23 -0500

    spacing formatting done

--- src/static/js/views.js
+++ src/static/js/views.js
@@ -41,8 +41,29 @@ var WordsView = Backbone.View.extend({
         this.listenTo(this.model, "change", this.render);
     },
 
+    format_text: function() {
+        /*
+         * Takes the list of words and properly spaces and concatenates
+         * them, pushing punctuation onto the end of the last word
+         */
+
+        var prev_words = this.model.get('prev_words');
+
+        for (var i = 0; i < prev_words.length; i++) {
+            var punctuation_match = prev_words[i].match(/[.?,!;:]+/);
+
+            if (!(punctuation_match != null &&
+                prev_words[i].length == punctuation_match[0].length)) {
+                prev_words[i] = " " + prev_words[i];
+            }
+        }
+
+        return prev_words
+    },
+
     render: function() {
-        this.$el.html(this.tpl(this.model.toJSON()));
+        var spaced_words = this.format_text();
+        this.$el.html(this.tpl({'prev_words': spaced_words}));
     }
 
 });
